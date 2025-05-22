d38ca1bc2f065f4fa43534ccd4da3dcc8f7a0090
cpplover0 <osdev555@yandex.ru>
2025-05-22 16:04:20 +0300

    :(

--- sysdeps/orange/generic/generic.cpp
+++ sysdeps/orange/generic/generic.cpp
@@ -233,12 +233,12 @@ int sys_clock_get(int clock, time_t *secs, long *nanos) {
 
 [[gnu::weak]] int sys_ioctl(int fd, unsigned long request, void *arg, int *result) {
    mlibc::infoLogger() << "TODO: Implement " << __func__ << frg::endlog;
-   return ENOSYS;
+   return 0;
 }
 
 [[gnu::weak]] int sys_fcntl(int fd, int request, va_list args, int *result) {
-   //mlibc::infoLogger() << "TODO: Implement " << __func__ << frg::endlog;
-   return ENOSYS;
+   mlibc::infoLogger() << "TODO: Implement " << __func__ << frg::endlog;
+   return 0;
 }
 
 [[gnu::weak]] int sys_dup(int fd, int flags, int *newfd) {
