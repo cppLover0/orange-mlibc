c8e60ca949d1299c290fc6b852cc8a86ee3bab93
cpplover0 <osdev555@yandex.com>
2025-05-25 16:26:55 +0300

    linux option

--- build_to_cross.sh
+++ build_to_cross.sh
@@ -1,5 +1,12 @@
 # first argument is link to orange dir
-meson setup build --cross-file ci/orange.cross-file --wipe --prefix=$1/initrd/usr
+
+git clone https://github.com/torvalds/linux.git --depth=1
+cd linux
+make headers_install ARCH=x86_64 INSTALL_HDR_PATH=$1/initrd/usr
+cd ..
+
+
+meson setup build --cross-file ci/orange.cross-file --wipe --prefix=$1/initrd/usr -Dlinux_kernel_headers=$1/initrd/usr/include
 cd build
 ninja -j$(nproc)
 ninja install 
