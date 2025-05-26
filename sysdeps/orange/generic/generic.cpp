7034053752a81b783b42d4b23032fbca41c3d822
cpplover0 <osdev555@yandex.com>
2025-05-26 11:20:07 +0300

    :(

--- sysdeps/orange/generic/generic.cpp
+++ sysdeps/orange/generic/generic.cpp
@@ -262,6 +262,7 @@ int sys_clock_get(int clock, time_t *secs, long *nanos) {
 }
 
 int sys_dup2(int fd, int flags, int newfd) {
+   mlibc::infoLogger() << "TODO: Implement " << __func__ << frg::endlog;
    return ENOSYS;
 }
 
