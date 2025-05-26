2abe2d322975bb820a3c8d530110ad39f8fdcdf2
cpplover0 <osdev555@yandex.com>
2025-05-26 09:18:42 +0300

    dup2

--- sysdeps/orange/generic/generic.cpp
+++ sysdeps/orange/generic/generic.cpp
@@ -262,8 +262,9 @@ int sys_clock_get(int clock, time_t *secs, long *nanos) {
 }
 
 int sys_dup2(int fd, int flags, int newfd) {
-   mlibc::infoLogger() << "TODO: Implement " << __func__ << frg::endlog;
-   return ENOSYS;
+   int ret;
+   asm volatile("syscall" : "=a"(ret) : "a"(28), "D"(fd), "S"(newfd));
+   return ret;
 }
 
 int sys_kill(int pid, int sig) {
