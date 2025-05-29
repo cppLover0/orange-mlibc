1c93125d24a0c0065efa7a56637af6cfc240c5e9
cpplover0 <osdev555@yandex.com>
2025-05-29 10:03:39 +0300

    sys_pipe

--- sysdeps/orange/generic/generic.cpp
+++ sysdeps/orange/generic/generic.cpp
@@ -321,5 +321,11 @@ int sys_kill(int pid, int sig) {
 
 }
 
+[[gnu::weak]] int sys_pipe(int *fds, int flags) {
+   int ret;
+   asm volatile("syscall" : "=a"(ret) : "a"(32), "D"(fds) : "rcx", "r11");
+   return ret;
+}
+
 
 }
