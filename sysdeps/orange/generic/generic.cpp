94f025452cbfaefe115d09f011d015dcd1606d45
cpplover0 <osdev555@yandex.com>
2025-11-11 15:23:53 +0300

    get out kid

--- sysdeps/orange/generic/generic.cpp
+++ sysdeps/orange/generic/generic.cpp
@@ -628,4 +628,8 @@ int sys_sigaction(int, const struct sigaction *__restrict, struct sigaction *__r
     return 0;
 }
 
+int sys_setitimer(int which, const struct itimerval *new_value, struct itimerval *old_value) {
+    return ENOSYS;
+}
+
 }
