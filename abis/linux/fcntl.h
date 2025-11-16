61469d02b23447491db9b5c0f4109ddf6bb5169c
cpplover0 <osdev555@yandex.com>
2025-11-16 17:51:24 +0300

    mlibc update

--- abis/linux/fcntl.h
+++ abis/linux/fcntl.h
@@ -104,6 +104,11 @@ struct f_owner_ex {
 
 #define F_OWNER_TID 0
 
+#define F_OWNER_PID 1
+
+
+#define F_OWNER_PGRP 2
+
 #define POSIX_FADV_NORMAL 0
 #define POSIX_FADV_RANDOM 1
 #define POSIX_FADV_SEQUENTIAL 2
