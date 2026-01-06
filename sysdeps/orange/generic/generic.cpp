9caaa157c54ae2f9cbe044655b7d25a9515ae603
cpplover0 <osdev555@yandex.com>
2026-01-06 13:43:30 +0300

    sys_fdatasync stuv

--- sysdeps/orange/generic/generic.cpp
+++ sysdeps/orange/generic/generic.cpp
@@ -630,6 +630,10 @@ int sys_sigaction(int, const struct sigaction *__restrict, struct sigaction *__r
     return 0;
 }
 
+int sys_fdatasync(int fd) {
+    return 0; // stub
+}
+
 int sys_setpriority(int which, id_t who, int prio) {
     int ret;
     asm volatile("syscall" : "=a"(ret) : "a"(59), "D"(which), "S"(who), "d"(prio) : "rcx", "r11");
