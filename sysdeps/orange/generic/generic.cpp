06b4ff6edaff8454d2fe017257b8c487f9e2af7d
cpplover0 <osdev555@yandex.com>
2026-01-08 13:29:46 +0300

    hi

--- sysdeps/orange/generic/generic.cpp
+++ sysdeps/orange/generic/generic.cpp
@@ -623,7 +623,7 @@ int sys_sigprocmask(int how, const sigset_t *__restrict set, sigset_t *__restric
 #ifndef MLIBC_BUILDING_RTLD
 
 void __mlibc_signalhandler(void (*jmp)(int sig), int signal, mcontext_t* ctx) {
-    mlibc::infoLogger() << "hi" << frg::endlog;
+    mlibc::infoLogger() << "hi " << (std::uint64_t)ctx << frg::endlog;
     uint64_t _jmp = (uint64_t)jmp;
     switch(_jmp) {
     case 0: {
