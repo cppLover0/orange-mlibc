1bb63518d11068d8b2d7e03208e579fc2a314b09
cpplover0 <osdev555@yandex.com>
2026-01-11 09:43:50 +0300

    disable linux option

--- build_to_cross.sh
+++ build_to_cross.sh
@@ -1,6 +1,6 @@
 # first argument is link to orange dir
 
-meson setup build --cross-file ci/orange.cross-file --wipe --prefix=$1/initrd/usr -Dlinux_kernel_headers=$1/initrd/usr/include
+meson setup build --cross-file ci/orange.cross-file --wipe --prefix=$1/initrd/usr -Dlinux_kernel_headers=$1/initrd/usr/include -Ddefault_library=both
 cd build
 ninja -j$(nproc)
 ninja install 
